e5e8f776061601d36ce8956deb3268e08ca2472b
hackesofice <amareight5@gmail.com>
2025-12-08 15:51:38 +0000

    initialization

--- main.js
+++ main.js
@@ -1,42 +1,75 @@
 "use strict";
 (() => {
-  const editorManager = acode.require("editorMnager");
-  const sideBarApps = acode.require("sidebarApps");
-  const SideButton = acode.require("sideButton");
+    const editorManager = acode.require("editorMnager");
+    const sideBarApps = acode.require("sidebarApps");
+    const SideButton = acode.require("sideButton");
 
-  var e = {
-    id: "offiicialkamal.pdfViewer",
-  };
+    var e = {
+        id: "pdfViewer",
+    };
 
-  class managerFunctions {
-    #currentFile() {
-      return editorManager.activeFile;
-    }
-  }
-
-  class eventsHandler {}
-
-  class pdfViewer {
-    constructor() {}
-    async init() {
-      editorManager.on("switch-file", () => {});
+    class managerFunctions {
+        #currentFile() {
+            return editorManager.activeFile;
+        }
     }
 
-    async destroy() {}
-  }
+    class eventsHandler {}
 
-  if (window.acode) {
-    let i = new pdfViewer();
-    acode.setPluginInit(
-      e.id,
-      async (n, o, { cacheFileUrl: s, cacheFile: d }) => {
-        n.endsWith("/") || (n += "/");
-        i.baseUrl = n;
-        await i.init(o, d, s);
-      }
-    );
-    acode.setPluginUnmount(e.id, () => {
-      i.destroy();
-    });
-  }
+    class pdfViewer {
+        constructor() {
+            this.currentEditorFilePath = null;
+            this.currentOpendFileName = null;
+        }
+        async init() {
+            // get the pdfjs later ill include it directly in project
+           // <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.2.67/pdf.min.js"></script>
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            // acode.registerFileHandler({id: e.id, extensions: ['pdf', 'xlsx', 'docx', 'csv'],
+            //     async handleFile( { name, uri, fs, options }) {
+            //         this.currentEditorFilePath = uri;
+            //         this.currentOpendFileName = name;
+            //         // Handler implementation
+            //         // open_Document(uri)
+            //         console.log('hsndler adder')
+            //     }
+            // });
+            
+        }
+
+        async destroy() {}
+    }
+
+    if (window.acode) {
+        let i = new pdfViewer();
+        acode.setPluginInit(
+            e.id,
+            async (n, o, {
+                cacheFileUrl: s, cacheFile: d
+            }) => {
+                n.endsWith("/") || (n += "/");
+                i.baseUrl = n;
+                await i.init(o, d, s);
+            }
+        );
+        acode.setPluginUnmount(e.id, () => {
+            i.destroy();
+        });
+    }
 })();
